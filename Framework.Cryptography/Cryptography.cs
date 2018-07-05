c9810cf500877090f6f7c952e492397fec84ff82
Jose Carlos de Jesus Junior <juninho_web@hotmail.com>
2018-07-05 16:12:53 -0300

    Add files via upload

---
+++ Framework.Cryptography/Cryptography.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Framework.Cryptography
+{
+    /// <summary>
+    /// Computes the System.Security.Cryptography.MD5 hash value for the input data
+    /// using the implementation provided by the cryptographic service provider (CSP).
+    /// This class cannot be inherited.
+    /// </summary>
+    public sealed class Cryptography
+    {
+        #region| Methods |
+
+        /// <summary>
+        /// Encrypt the given string using the specified key.
+        /// </summary>
+        /// <param name="textToEncrypt">The string to be encrypted</param>
+        /// <param name="SecurePhrase">The Secure Phrase used to encryption</param>
+        /// <returns>The Encrypted string.</returns>
+        public static string Encrypt(string textToEncrypt, string SecurePhrase)
+        {
+            try
+            {
+                var oTripleDESCryptoServiceProvider = new TripleDESCryptoServiceProvider();
+                var oMD5CryptoServiceProvider       = new MD5CryptoServiceProvider();
+
+                byte[] byteHash, byteBuff;
+
+                byteHash = oMD5CryptoServiceProvider.ComputeHash(ASCIIEncoding.ASCII.GetBytes(SecurePhrase));
+
+                oTripleDESCryptoServiceProvider.Key  = byteHash;
+                oTripleDESCryptoServiceProvider.Mode = CipherMode.ECB; //CBC, CFB
+
+                byteBuff = ASCIIEncoding.ASCII.GetBytes(textToEncrypt);
+
+                string Aux = Convert.ToBase64String(oTripleDESCryptoServiceProvider.CreateEncryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+
+                oTripleDESCryptoServiceProvider.Dispose();
+                oMD5CryptoServiceProvider.Dispose();
+
+                return Aux;
+
+            }
+            catch (Exception ex)
+            {
+                return "Framework.Cryptography: " + ex.Message;
+            }
+        }
+
+        /// <summary>
+        /// Decrypt the given string using the specified key.
+        /// </summary>
+        /// <param name="textToDecrypt">The string to be encrypted</param>
+        /// <param name="SecurePhrase">The Secure Phrase used to decryption</param>
+        /// <returns>The Decrypted string.</returns>
+        public static string Decrypt(string textToDecrypt, string SecurePhrase)
+        {
+            try
+            {
+                var oTripleDESCryptoServiceProvider = new TripleDESCryptoServiceProvider();
+                var oMD5CryptoServiceProvider = new MD5CryptoServiceProvider();
+
+                byte[] byteHash, byteBuff;
+
+                byteHash = oMD5CryptoServiceProvider.ComputeHash(ASCIIEncoding.ASCII.GetBytes(SecurePhrase));
+
+              
+                oTripleDESCryptoServiceProvider.Key = byteHash;
+                oTripleDESCryptoServiceProvider.Mode = CipherMode.ECB; //CBC, CFB
+
+                byteBuff = Convert.FromBase64String(textToDecrypt);
+                
+                string Aux = ASCIIEncoding.ASCII.GetString(oTripleDESCryptoServiceProvider.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+
+                oTripleDESCryptoServiceProvider.Dispose();
+                oMD5CryptoServiceProvider.Dispose();
+
+                return Aux;
+            }
+            catch (Exception ex)
+            {
+                return "Framework.Cryptography: " + ex.Message;
+            }
+        }
+
+        /// <summary>
+        /// Encrypts a given string using the ProtectedConfigurationProvider
+        /// </summary>
+        public static string EncryptUsingTripleDES(string encryptValue)
+        {
+            var output = string.Empty;
+
+            var tripleDES = new TripleDESProtectedConfigurationProvider();
+
+            output = tripleDES.EncryptString(encryptValue);
+
+            tripleDES = null;
+
+            return output;
+        }
+
+        /// <summary>
+        /// Decrypts a given string using the ProtectedConfigurationProvider
+        /// </summary>
+        public static string DecryptUsingTripleDES(string textToDecrypt)
+        {
+            var output = string.Empty;
+
+            var tripleDES = new TripleDESProtectedConfigurationProvider();
+
+            output = tripleDES.DecryptString(textToDecrypt);
+
+            tripleDES = null;
+
+            return output;
+        }
+
+        /// <summary>
+        /// Generates a unique key to be used as a token
+        /// </summary>
+        /// <param name="maxSize"></param>
+        /// <returns></returns>
+        public static string GenerateTokenUniqueKey(int maxSize = 15)
+        {
+            char[] chars = new char[62];
+            chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+            byte[] data = new byte[1];
+
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetNonZeroBytes(data);
+                data = new byte[maxSize];
+                crypto.GetNonZeroBytes(data);
+            }
+
+            var result = new StringBuilder(maxSize);
+
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length)]);
+            }
+
+            return result.ToString();
+        }
+
+
+        #endregion
+    }
+}
---
+++ Framework.Cryptography/TripleDESProtectedConfigurationProvider.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+
+namespace Framework.Cryptography
+{
+    /// <summary>
+    /// Web Config security with ProtectedConfigurationProvider
+    /// </summary>
+    public class TripleDESProtectedConfigurationProvider : ProtectedConfigurationProvider
+    {
+        #region| Fields |
+
+        private TripleDESCryptoServiceProvider tripleDesCripto = new TripleDESCryptoServiceProvider();
+        private string key = "Sgo0k8dv112TCU0BiMNOo0tfR5AYmXeC";
+        private string IV = "mmVbNIIj8k4=";
+
+        #endregion
+
+        #region| Constructor |
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TripleDESProtectedConfigurationProvider()
+        {
+            //Lê a chave e o IV
+            tripleDesCripto.Key = Convert.FromBase64String(key);
+            tripleDesCripto.IV = Convert.FromBase64String(IV);
+            //ECB(Electronic code Book)
+
+            tripleDesCripto.Mode = CipherMode.ECB;
+            //padding mode se tiver qualquer byte adicional a ser adicionado
+            tripleDesCripto.Padding = PaddingMode.PKCS7;
+        }
+
+        #endregion
+
+        #region| Methods |
+
+        /// <summary>
+        /// Encrypt the web.config file
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public override XmlNode Encrypt(XmlNode node)
+        {
+
+            string encryptedData = EncryptString(node.OuterXml);
+
+            // Creates a encrypted section in thw Web Config file
+            var xmlDoc = new XmlDocument();
+
+            xmlDoc.PreserveWhitespace = true;
+            xmlDoc.LoadXml("<EncryptedData>" + encryptedData + "</EncryptedData>");
+            return xmlDoc.DocumentElement;
+        }
+
+        /// <summary>
+        /// Decrypt the web.config file
+        /// </summary>
+        /// <param name="encryptedNode"></param>
+        /// <returns></returns>
+        public override XmlNode Decrypt(XmlNode encryptedNode)
+        {
+            string decryptedData = DecryptString(encryptedNode.InnerText);
+
+            // Decrypt the Web.Config section
+            var xmlDoc = new XmlDocument();
+
+            xmlDoc.PreserveWhitespace = true;
+            xmlDoc.LoadXml(decryptedData);
+
+            return xmlDoc.DocumentElement;
+        }
+
+        /// <summary>
+        /// Encrypts a given string using the System.Security.Cryptography.MD5 hash value for the input data
+        /// using the implementation provided by the cryptographic service provider (CSP).
+        /// This class cannot be inherited.
+        /// </summary>
+        internal string EncryptString(string encryptValue)        {
+
+            var valBytes = Encoding.Unicode.GetBytes(encryptValue);
+
+            var transform = tripleDesCripto.CreateEncryptor();
+
+            var ms = new MemoryStream();
+            var cs = new CryptoStream(ms, transform, CryptoStreamMode.Write);
+
+            cs.Write(valBytes, 0, valBytes.Length);
+            cs.FlushFinalBlock();
+
+            byte[] returnBytes = ms.ToArray();
+            cs.Close();
+
+            return Convert.ToBase64String(returnBytes);
+        }
+
+        /// <summary>
+        /// Decrypts a given string
+        /// </summary>
+        internal string DecryptString(string encryptedValue)
+        {
+            var valBytes = Convert.FromBase64String(encryptedValue);
+
+            var transform = tripleDesCripto.CreateDecryptor();
+
+            var ms = new MemoryStream();
+            var cs = new CryptoStream(ms, transform, CryptoStreamMode.Write);
+
+            cs.Write(valBytes, 0, valBytes.Length);
+            cs.FlushFinalBlock();
+
+            byte[] returnBytes = ms.ToArray();
+            cs.Close();
+
+            return Encoding.Unicode.GetString(returnBytes);
+        } 
+
+        #endregion
+
+    }
+}
